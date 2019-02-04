d8bd7154bad925ba5e3d5286a3ff1acb02510ed0
Ammar Ali <funnyguy118@gmail.com>
2019-02-04 21:10:29 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,25 +1,36 @@
 require 'pry'
 
 def get_first_name_of_season_winner(data, season)
+  
   data.each do |season_num, array_of_hashes|
     
     array_of_hashes.each_with_index do |contestant_hash, index|
+     
       contestant_hash.each do |attribute, attribute_value|
+          
           if season_num == season && attribute_value == "Winner"
             return data[season_num][index]["name"].split.first
           end
-          
+
       end
-    
     end
-  
-  
   end
-  
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.each do |season_num, array_of_hashes|
+    
+    array_of_hashes.each_with_index do |contestant_hash, index|
+     
+      contestant_hash.each do |attribute, attribute_value|
+          
+          if attribute_value == occupation
+            return data[season_num][index]["name"].split.first
+          end
+
+      end
+    end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
