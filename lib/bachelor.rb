66980e74865b1e168c41ae01be0fbd7a16e8791b
Ammar Ali <funnyguy118@gmail.com>
2019-02-04 20:49:42 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -2,13 +2,28 @@ require 'pry'
 let(:data) { JSON.parse(File.read('spec/fixtures/contestants.json')) }
 
 def get_first_name_of_season_winner(data, season)
-  #data.each do |season_number, contestant_info|
-  #end
   
-  puts data[season]["status"]
-  binding pry 
+  data.each do |season_number, contestant_info|
+    
+    if season_number == season 
+      
+      contestant_info.each do |attribute, attribute_data|
+        
+        if data[season_number]["status"] == "Winner"
+          puts data[season_number]["name"]
+        else 
+          puts "Winner Failed"  
+        end
+      
+      end
+      
+    else 
+      puts "Season Failed"
+    
+    end
   
-  
+  end
+
 end
 
 def get_contestant_name(data, occupation)
