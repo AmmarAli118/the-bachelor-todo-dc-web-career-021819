e208906dd0b41ba4d24495663df09cd135997ffd
Ammar Ali <funnyguy118@gmail.com>
2019-02-04 21:03:47 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,7 +1,20 @@
 require 'pry'
 
 def get_first_name_of_season_winner(data, season)
+  data.each do |season_num, array_of_hashes|
+    
+    array_of_hashes.each do |contestant_hash|
+      contestant_hash.each do |attribute, attribute_value|
+          if season_num == season && attribute_value == "Winner"
+            puts "Yay"
+          end
+          
+      end
+    
+    end
   
+  
+  end
   
 end
 
