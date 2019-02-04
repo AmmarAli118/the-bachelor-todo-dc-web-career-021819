0087ced5a270d76ade017b4cb9536cb3ea5e5736
Ammar Ali <funnyguy118@gmail.com>
2019-02-04 20:55:34 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -2,27 +2,7 @@ require 'pry'
 
 def get_first_name_of_season_winner(data, season)
   
-  data.each do |season_number, contestant_info|
-    
-    if season_number == season 
-      
-      contestant_info.each do |attribute, attribute_data|
-        
-        if data[season_number]["status"] == "Winner"
-          puts data[season_number]["name"]
-        else 
-          puts "Winner Failed"  
-        end
-      
-      end
-      
-    else 
-      puts "Season Failed"
-    
-    end
   
-  end
-
 end
 
 def get_contestant_name(data, occupation)
